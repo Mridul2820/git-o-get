595711b6a4368c0b8c1ae921c22d36a60da11e08
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-09 17:16:19 +0530

    other packages install & recoil setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <RecoilRoot>
+            <Component {...pageProps} />
+        </RecoilRoot>
+    )
 }
 
 export default MyApp
