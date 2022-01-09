0c510f8bacf86fa6d13830b2b2c43f4b8bbf384b
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-09 17:06:52 +0530

    app clean up

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+const path = require('path')
+require('dotenv').config()
+
 module.exports = {
-  reactStrictMode: true,
+    env: { },
+    publicRuntimeConfig: {},
+    images: {
+        formats: [
+            'image/avif', 
+            'image/webp'
+        ],
+        domains: [
+            'localhost', 
+            'res.cloudinary.com',
+            'inneedstore.com',
+            'i0.wp.com',
+            'images.pexels.com'
+        ]
+    },
+
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['public'] = path.join(__dirname, 'public')
+
+        return config
+    },
 }
