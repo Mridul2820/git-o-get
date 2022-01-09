70594b7f6b0deaa9a8617b7aa734df95815007f3
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-09 17:22:41 +0530

    seo setup

---
+++ next-seo.config.js
@@ -0,0 +1,18 @@
+export default {
+    title: "Get Your git",
+    description: "Get Your git",
+    openGraph: {
+        type: 'website',
+        locale: 'en_IN',
+        url: 'mrx.com',
+        site_name: 'Get Your git',
+        // images: [
+        //     {
+        //         url: "https://res.cloudinary.com/sportzhut/image/upload/v1627916090/logo_d80ajt.png",
+        //         width: 300,
+        //         height: 300,
+        //         alt: "Get Your git",
+        //     }
+        // ],
+    }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 
+// SEO
+import { DefaultSeo } from 'next-seo'
+import SEO from '../next-seo.config'
+
 function MyApp({ Component, pageProps }) {
     return (
         <RecoilRoot>
+            <DefaultSeo {...SEO} />
             <Component {...pageProps} />
         </RecoilRoot>
     )
