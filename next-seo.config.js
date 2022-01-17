7653ac27f509d2a0cd0ccb1742d0a4df172abac6
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-17 18:50:49 +0530

    over all seo update

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,21 @@
 export default {
-    title: "Git O Get - Github Profile in a Different way",
-    description: "View Your Github Profile in a Different and Graphical way",
+    title: "Git O Get - Github Profile Stats and Graphs in One Place",
+    description: "Github Profile Stats, Languge Graph, Social Card, Contribution Graph, Repository Stats and Graphs",
     openGraph: {
-        title: "Git O Get - Github Profile in a Different way",
-        description: "View Your Github Profile in a Different and Graphical way",
+        title: "Git O Get - Github Profile Stats and Graphs in One Place",
+        description: "Github Profile Stats, Languge Graph, Social Card, Contribution Graph, Repository Stats and Graphs",
         type: 'website',
         locale: 'en_IN',
         url: 'https://git-to-get.vercel.app',
         site_name: 'git-to-get',
+        twitter: {
+            cardType: 'summary_large_image',
+        },
         images: [
             {
-                url: "https://res.cloudinary.com/dbxcernxw/image/upload/v1641915729/app_logos/logo_mty5eq.png",
-                width: 300,
-                height: 300,
+                url: "https://res.cloudinary.com/dbxcernxw/image/upload/v1642425441/git-o-get/ogimage_mmpaia.jpg",
+                width: 2024,
+                height: 1012,
                 alt: "Get Your git",
             }
         ],
