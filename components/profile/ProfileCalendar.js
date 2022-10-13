9b1eb644a6aaa4a7be23a55784916a22ead8b453
xnatsuk <xnatsuk@gmail.com>
2022-10-13 20:51:50 -0300

    fix: update profile calendar to use dark theme

--- components/profile/ProfileCalendar.js
+++ components/profile/ProfileCalendar.js
@@ -1,8 +1,40 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
 import styled from 'styled-components';
+import { useTheme } from 'next-themes';
+
+const useColorTheme = () => {
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  if (currentTheme === 'dark') {
+    return {
+      level4: '#39d353',
+      level3: '#26a641',
+      level2: '#006d32',
+      level1: '#0e4429',
+      level0: '#161b22',
+    };
+  } else {
+    return {
+      level4: '#216e39',
+      level3: '#30a14e',
+      level2: '#40c463',
+      level1: '#9be9a8',
+      level0: '#ebedf0',
+    };
+  }
+};
 
 const ProfileCalendar = ({ username }) => {
   return (
@@ -16,7 +48,7 @@ const ProfileCalendar = ({ username }) => {
         <a href={`https://github.com/${username}`}>{'@' + username}</a> on
         GitHub
       </p>
-      <GitHubCalendar username={username} fontSize={18}>
+      <GitHubCalendar username={username} fontSize={18} theme={useColorTheme()}>
         <ReactTooltip delayShow={50} html />
       </GitHubCalendar>
     </CalenderWrap>
