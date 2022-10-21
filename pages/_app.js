57e644e0a5d8a1af3dc3d56b996dc103d5b4a534
zainbinfurqan <zain.ahmed199524@gmail.com>
2022-10-22 02:11:03 +0500

    update loading issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React, { useReducer } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
@@ -12,19 +13,40 @@ import { ApolloProvider } from '@apollo/client';
 import { client } from '../client';
 
 import Footer from '../components/footer/Footer';
+export const AppContext = React.createContext();
+
+const reducer = (state, action) => {
+  if (action.type === 'LOADING_TOGGLE') {
+    return { ...state, isLoading: action.payload };
+  }
+  return state;
+};
+const initialStates = { isLoading: false }
 
 function MyApp({ Component, pageProps }) {
+
+  const [state, dispatch] = useReducer(reducer, initialStates)
+
+  const toggleLoading = async (flag) => {
+    dispatch({
+      type: 'LOADING_TOGGLE',
+      payload: flag
+    });
+  }
+
   return (
     <ApolloProvider client={client}>
       <RecoilRoot>
         <DefaultSeo {...SEO} />
         <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-          <ThemeButton />
-          <Footer />
+          <AppContext.Provider value={{ ...state, toggleLoading }}>
+            <Component {...pageProps} />
+            <ThemeButton />
+            <Footer />
+          </AppContext.Provider>
         </ThemeProvider>
       </RecoilRoot>
-    </ApolloProvider>
+    </ApolloProvider >
   );
 }
 
