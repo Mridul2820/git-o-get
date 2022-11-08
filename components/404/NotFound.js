6f0b763f7f5e28932aba0e752dc428809cb1cef1
Mridul2820 <immridulpanda2820@gmail.com>
2022-11-08 21:47:15 +0530

    next 13 upgrade

--- components/404/NotFound.js
+++ components/404/NotFound.js
@@ -11,11 +11,12 @@ const NotFound = ({ image, message }) => {
         className="w-full max-w-[500px]"
       />
       <p className="text-center">{message}</p>
-      <Link href="/">
-        <a className="bg-purple-mid flex justify-center items-center gap-2 text-white px-5 py-2 text-lg rounded-lg">
-          <BiArrowBack />
-          Back to home
-        </a>
+      <Link
+        href="/"
+        className="bg-purple-mid flex justify-center items-center gap-2 text-white px-5 py-2 text-lg rounded-lg"
+      >
+        <BiArrowBack />
+        Back to home
       </Link>
     </div>
   );
--- components/profile/ProfileFollowers.js
+++ components/profile/ProfileFollowers.js
@@ -12,25 +12,27 @@ const ProfileFollowers = ({ username, followers }) => {
       </p>
       <div className="flex flex-col gap-4 max-h-[450px] overflow-y-scroll pt-2 pr-5">
         {followers?.nodes.map((follow) => (
-          <Link href={`/user/${follow.login}`} key={follow.id}>
-            <a className="dark:bg-dark-gray dark:border-gray-700 flex items-center gap-3 rounded-md shadow-bs5 hover:shadow-bs2 transition-all duration-200 p-2 border-2 border-slate-300">
-              <img
-                src={follow.avatarUrl}
-                alt={follow.login}
-                height={40}
-                width={40}
-                className="rounded-full"
-              />
-              <div className="">
-                <p className="text-base font-semibold">
-                  {'@'}
-                  {follow.login}
-                </p>
-                <p className="font-normal text-slate-600 text-sm dark:text-slate-400">
-                  {follow.name}
-                </p>
-              </div>
-            </a>
+          <Link
+            href={`/user/${follow.login}`}
+            key={follow.id}
+            className="dark:bg-dark-gray dark:border-gray-700 flex items-center gap-3 rounded-md shadow-bs5 hover:shadow-bs2 transition-all duration-200 p-2 border-2 border-slate-300"
+          >
+            <img
+              src={follow.avatarUrl}
+              alt={follow.login}
+              height={40}
+              width={40}
+              className="rounded-full"
+            />
+            <div className="">
+              <p className="text-base font-semibold">
+                {'@'}
+                {follow.login}
+              </p>
+              <p className="font-normal text-slate-600 text-sm dark:text-slate-400">
+                {follow.name}
+              </p>
+            </div>
           </Link>
         ))}
       </div>
