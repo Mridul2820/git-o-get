210e1799ac8883f578072863548054333ab01ab4
Mridul2820 <immridulpanda2820@gmail.com>
2023-04-30 23:20:05 +0530

    Pnned repo update

--- components/profile/PinnedRepositories.js
+++ components/profile/PinnedRepositories.js
@@ -6,7 +6,7 @@ const PinnedRepositories = ({ username, pinnedRepositories }) => {
   const { theme } = useTheme();
 
   return (
-    <div className="dark:bg-dark-color dark:text-white dark:border dark:border-gray-700 p-3 shadow-bs1 rounded-md h-full w-full bg-white">
+    <div className="card-item p-5">
       <p className="text-center mb-5 text-xl font-semibold">
         <div>
           {"@"} {username} Pinned Repositories
