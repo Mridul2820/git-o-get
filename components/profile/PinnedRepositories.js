878551ab214a69acdd569865a1c10bebcb071414
Mridul2820 <immridulpanda2820@gmail.com>
2023-09-05 12:48:10 +0530

    Hydration Error Resolve

--- components/profile/PinnedRepositories.js
+++ components/profile/PinnedRepositories.js
@@ -10,9 +10,7 @@ const PinnedRepositories = ({ username, pinnedRepositories }) => {
   return (
     <div className="card-item p-5">
       <p className="text-center mb-5 text-xl font-semibold">
-        <div>
-          {"@"} {username} Pinned Repositories
-        </div>
+        {"@"} {username} Pinned Repositories
       </p>
       <div className="flex flex-wrap justify-center gap-4 max-h-[450px] overflow-y-scroll pt-2 pr-5">
         {pinnedRepositories?.nodes.map((repo) => (
